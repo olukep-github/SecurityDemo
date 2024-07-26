1f226b5cfff8eb964f8f755fcf460331b6250720
olukep <1277019466@qq.com>
2024-07-26 09:40:30 +0800

    LoginController添加

--- src/main/java/com/bjca/securitydemo/handler/MyAuthenticationFailureHandler.java
+++ src/main/java/com/bjca/securitydemo/handler/MyAuthenticationFailureHandler.java
@@ -5,7 +5,6 @@ import org.springframework.security.web.authentication.AuthenticationFailureHand
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
